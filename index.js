32ebccb5c1ef02363552bb7e7c24e9d8afc16c03
Kyle Campbell <mail@slajax.com>
2013-06-29 14:19:15 -0700

    changed dev websocket endpoint

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var Retsly = module.exports = exports = (function() {
   var Client = function(api_key, options) {
     this.api_key = api_key;
     this.options = _.extend({ urlBase: '', debug: false }, options);
-    var host = (typeof RETSLY_CONF != "undefined" && RETSLY_CONF.env === 'development') ? 'dev.rets.ly' : 'rets.ly';
+    var host = (typeof RETSLY_CONF != "undefined" && RETSLY_CONF.env === 'development') ? 'localhost:3000' : 'rets.ly';
     this.io = io.connect('http://'+host+'/');
     _this = this;
     return this;
