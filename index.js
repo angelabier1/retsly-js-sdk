4fe68dc2a4177e0d208086776b6c45dae2da61b3
Kyle Campbell <mail@slajax.com>
2013-07-05 12:16:12 -0700

    changes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var Retsly = module.exports = exports = (function() {
   var Client = function(api_key, options) {
     this.api_key = api_key;
     this.options = _.extend({ urlBase: '/api/v1', debug: false }, options);
-    this.host = (typeof RETSLY_CONF != "undefined" && RETSLY_CONF.env === 'development') ? 'localhost:3000' : 'rets.ly';
+    this.host = (typeof RETSLY_CONF != "undefined" && RETSLY_CONF.env === 'development') ? 'localhost:3000' : 'rets.io';
     this.io = io.connect('http://'+this.host+'/');
     this.init_stack = [];
     this.init();
