068494daabb9e8b80d6d651e1ae8373db75f2c38
Kyle Campbell <mail@slajax.com>
2013-10-07 13:44:12 -0700

    sdk needs to be able to set cookies too

--- index.js
+++ index.js
@@ -121,6 +121,17 @@ var Retsly = module.exports = exports = (function() {
     return cookies[name];
   };
 
+  Client.prorotype.setCookie = function (name,value,days) {
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime()+(days*24*60*60*1000));
+      var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+  };
+
+
 /*
  * Retsly Backbone sync over websockets
  */
