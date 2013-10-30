7ca39a7d8041a3499e261113f5211eaf93f0fa74
Kyle Campbell <mail@slajax.com>
2013-10-29 22:49:05 -0700

    force sdk to use ssl in dev mode

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function protocol() { return window.location.protocol + '//'; }
+function protocol() { return 'https://'; }
 
 var Retsly = module.exports = exports = (function() {
 
@@ -15,7 +15,7 @@ var Retsly = module.exports = exports = (function() {
 
     this.client_id = client_id, this.token = null;
     this.options = _.extend({ urlBase: '/api/v1', debug: false }, options);
-    this.host = (RETSLY_CONF && RETSLY_CONF.env === 'development') ? 'localhost:3000' : 'rets.io:443';
+    this.host = (RETSLY_CONF && RETSLY_CONF.env === 'development') ? 'dev.rets.io:443' : 'rets.io:443';
     this.io = io.connect(protocol()+this.host+'/', {'sync disconnect on unload':false});
     this.init_stack = [];
     this.init();
