b2234ee41797d357b657c6f54adc94b222e35fd2
Luke Vivier <luke@vivier.ca>
2013-11-18 15:42:07 -0800

    ocd

--- index.js
+++ index.js
@@ -21,65 +21,48 @@ module.exports = Retsly;
  */
 var _this;
 function Retsly (client_id, options) {
-  if(!client_id)
+  if (!client_id)
     throw new Error('You must provide a client_id - ie: new Retsly(\'xxx\');');
 
   this.client_id = client_id, this.token = null;
   this.options = extend({ urlBase: '/api/v1', debug: false }, options);
   this.host = (document.domain.indexOf('dev.rets.ly') > -1) ? 'dev.rets.io:443' : 'rets.io:443';
   this.io = io.connect(PROTOCOL+this.host+'/', {'sync disconnect on unload':false});
-  this.init_stack = [];
+
+  // debug mode
+  Retsly.debug = this.options.debug || false;
+
+  this.__init_stack = [];
   this.init();
   _this = this;
-  return this;
-};
+}
 
 Retsly.prototype.init = function() {
-
   var self = this;
-
-  if($(document.body).hasClass('retsly')) return this.ready();
-  if(this.options.debug) console.log('--> Loading Retsly SDK...');
-
-  var css = $('<link>').attr({
-    media: 'all', rel: 'stylesheet',
-    href: PROTOCOL+self.host+'/css/sdk'
+  debug('--> Loading Retsly SDK...');
+  // <!-- Make sure you ask @slajax before changing this
+  ajax({
+    type: 'POST',
+    xhrFields: { withCredentials: true },
+    data: { origin: document.location.protocol+'//'+document.domain, action: 'set' },
+    url: PROTOCOL+self.host+'/api/v1/session?origin='+document.domain,
+    success: function(sid) {
+      self.io.emit('authorize', { sid: sid }, function(data) {
+        if(typeof data.bundle === 'string') self.setCookie('retsly.sid', data.bundle);
+        debug('<-- Retsly SDK Loaded!');
+        self.ready();
+      });
+    }
   });
-
-  css.load(function() {
-    self.get('/api/v1/templates', {}, function(res) {
-      if(self.options.debug) console.log('<-- Retsly SDK Loaded!');
-      if(res.success) {
-        $(document.body).addClass('retsly').append('<div id="retsly-templates" />');
-        $('#retsly-templates').append(res.bundle);
-
-        // <!-- Make sure you ask @slajax before changing this
-        $.ajax({
-          type: 'POST', xhrFields: { withCredentials: true },
-          data: { origin: document.location.protocol+'//'+document.domain, action: 'set' },
-          url: PROTOCOL+self.host+'/api/v1/session?origin='+document.domain,
-          success: function(sid) {
-            self.io.emit('authorize', { sid: sid }, function(data) {
-              if(typeof data.bundle === 'string') self.setCookie('retsly.sid', data.bundle);
-              self.ready();
-            });
-          }
-        });
-        // Make sure you ask @slajax before changing this -->
-      }
-    });
-
-  });
-
-  css.appendTo('head');
-
+  // Make sure you ask @slajax before changing this -->
 };
 
 //TODO kyle: Make this restful. It's way too fucken late right now.
 Retsly.prototype.logout = function(cb) {
   var success = cb || function() {};
-  $.ajax({
-    type: 'POST', xhrFields: { withCredentials: true },
+  ajax({
+    type: 'POST',
+    xhrFields: { withCredentials: true },
     data: { origin: document.location.protocol+'//'+document.domain, action: 'del' },
     url: PROTOCOL+this.host+'/api/v1/session',
     error: function(error) { throw new Error(error); },
@@ -97,8 +80,8 @@ Retsly.prototype.getToken = function() {
 };
 
 Retsly.prototype.ready = function(cb) {
-  if(cb) this.init_stack.push(cb);
-  else each(this.init_stack, function(c) { if(typeof c === 'function') c(); });
+  if(cb) this.__init_stack.push(cb);
+  else each(this.__init_stack, function(c) { if(typeof c === 'function') c(); });
 };
 
 Retsly.prototype.request = function(url, options, cb) {
@@ -109,7 +92,7 @@ Retsly.prototype.get = function(url, query, cb) {
 
   var options = {};
   options.method = 'get';
-  options.query = typeof query === "object" ? query : {};
+  options.query = typeof query === 'object' ? query : {};
   options.query.client_id = this.client_id;
 
   if(this.getToken())
@@ -121,7 +104,7 @@ Retsly.prototype.get = function(url, query, cb) {
 Retsly.prototype.post = function(url, body, cb) {
   var options = {};
   options.method = 'post';
-  options.body =  typeof body === "object" ? body : {};
+  options.body =  typeof body === 'object' ? body : {};
   options.query = { client_id: this.client_id };
 
   if(this.getToken())
@@ -133,7 +116,7 @@ Retsly.prototype.post = function(url, body, cb) {
 Retsly.prototype.put = function(url, body, cb) {
   var options = {};
   options.method = 'put';
-  options.body =  typeof body === "object" ? body : {};
+  options.body =  typeof body === 'object' ? body : {};
   options.query = { client_id: this.client_id };
 
    if(this.getToken())
@@ -145,7 +128,7 @@ Retsly.prototype.put = function(url, body, cb) {
 Retsly.prototype.del = function(url, body, cb) {
   var options = {};
   options.method = 'delete';
-  options.body =  typeof body === "object" ? body : {};
+  options.body =  typeof body === 'object' ? body : {};
   options.query = { client_id: this.client_id };
 
   if(this.getToken())
@@ -182,8 +165,16 @@ Retsly.prototype.setCookie = function (name,value,days) {
   if (days) {
     var date = new Date();
     date.setTime(date.getTime()+(days*24*60*60*1000));
-    var expires = "; expires="+date.toGMTString();
+    var expires = '; expires='+date.toGMTString();
   }
-  else var expires = "";
-  document.cookie = name+"="+value+expires+"; path=/";
+  else var expires = '';
+  document.cookie = name+'='+value+expires+'; path=/';
 };
+
+
+/**
+ * Log only if debug mode
+ */
+function debug () {
+  if (Retsly.debug) console.log.apply(console, arguments);
+}
--- test/index.js
+++ test/index.js
@@ -12,23 +12,21 @@ var assert = require('assert');
 suite('Retsly');
 
 test('is a constructor', function () {
-  assert('function' == typeof Retsly, 'is a function');
+  assert('function' == typeof Retsly);
 });
 
 test('expects two arguments', function () {
   assert(2 == Retsly.length);
-  assert.throws(function(){new Retsly()}, 'client_id param is required');
-  assert.ok(new Retsly('test'), 'options param is optional');
+  assert.throws(function(){new Retsly()});
+  assert.ok(new Retsly('test'));
 });
 
 test('has public options property', function () {
   var r = new Retsly('test', {foo: 'bar'});
-  assert('bar' == r.options.foo, 'public options property');
+  assert('bar' == r.options.foo);
 });
 
 test('has a socket.io connection', function () {
-  var r = new Retsly('test')
-  assert(r.io)
+  var r = new Retsly('test');
+  assert(r.io);
 });
-
-
