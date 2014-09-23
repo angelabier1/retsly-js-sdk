b4cf0ec5a321d1f1df5cc5ad5e36b9f33f694878
Kyle Campbell <mail@slajax.com>
2014-09-23 09:33:19 -0700

    posts do not work right now - this fixes it

--- retsly.js
+++ retsly.js
@@ -4693,116 +4693,6 @@ if (typeof define === "function" && define.amd) {
 })();
 });
 
-require.register("yields~unserialize@0.0.1", function (exports, module) {
-
-/**
- * Unserialize the given "stringified" javascript.
- * 
- * @param {String} val
- * @return {Mixed}
- */
-
-module.exports = function(val){
-  try {
-    return JSON.parse(val);
-  } catch (e) {
-    return val || undefined;
-  }
-};
-
-});
-
-require.register("yields~store@0.2.0", function (exports, module) {
-
-/**
- * dependencies.
- */
-
-var each = require('component~each@0.2.5')
-  , unserialize = require('yields~unserialize@0.0.1')
-  , storage = window.localStorage
-  , type = require('component~type@1.0.0');
-
-/**
- * Store the given `key` `val`.
- *
- * @param {String} key
- * @param {Mixed} val
- * @return {Mixed}
- */
-
-exports = module.exports = function(key, val){
-  switch (arguments.length) {
-    case 2: return set(key, val);
-    case 0: return all();
-    case 1: return 'object' == type(key)
-      ? each(key, set)
-      : get(key);
-  }
-};
-
-/**
- * supported flag.
- */
-
-exports.supported = !! storage;
-
-/**
- * export methods.
- */
-
-exports.set = set;
-exports.get = get;
-exports.all = all;
-
-/**
- * Set `key` to `val`.
- *
- * @param {String} key
- * @param {Mixed} val
- */
-
-function set(key, val){
-  return null == val
-    ? storage.removeItem(key)
-    : storage.setItem(key, JSON.stringify(val));
-}
-
-/**
- * Get `key`.
- *
- * @param {String} key
- * @return {Mixed}
- */
-
-function get(key){
-  return null == key
-    ? storage.clear()
-    : unserialize(storage.getItem(key));
-}
-
-/**
- * Get all.
- *
- * @return {Object}
- */
-
-function all(){
-  var len = storage.length
-    , ret = {}
-    , key
-    , val;
-
-  for (var i = 0; i < len; ++i) {
-    key = storage.key(i);
-    ret[key] = get(key);
-  }
-
-  return ret;
-}
-
-});
-
 require.register("retsly~ajax@0.0.3", function (exports, module) {
 var type
 try {
@@ -5092,6 +4982,116 @@ function extend(target) {
         target[key] = source[key]
   })
   return target
+}
+
+});
+
+require.register("yields~unserialize@0.0.1", function (exports, module) {
+
+/**
+ * Unserialize the given "stringified" javascript.
+ * 
+ * @param {String} val
+ * @return {Mixed}
+ */
+
+module.exports = function(val){
+  try {
+    return JSON.parse(val);
+  } catch (e) {
+    return val || undefined;
+  }
+};
+
+});
+
+require.register("yields~store@0.2.0", function (exports, module) {
+
+/**
+ * dependencies.
+ */
+
+var each = require('component~each@0.2.5')
+  , unserialize = require('yields~unserialize@0.0.1')
+  , storage = window.localStorage
+  , type = require('component~type@1.0.0');
+
+/**
+ * Store the given `key` `val`.
+ *
+ * @param {String} key
+ * @param {Mixed} val
+ * @return {Mixed}
+ */
+
+exports = module.exports = function(key, val){
+  switch (arguments.length) {
+    case 2: return set(key, val);
+    case 0: return all();
+    case 1: return 'object' == type(key)
+      ? each(key, set)
+      : get(key);
+  }
+};
+
+/**
+ * supported flag.
+ */
+
+exports.supported = !! storage;
+
+/**
+ * export methods.
+ */
+
+exports.set = set;
+exports.get = get;
+exports.all = all;
+
+/**
+ * Set `key` to `val`.
+ *
+ * @param {String} key
+ * @param {Mixed} val
+ */
+
+function set(key, val){
+  return null == val
+    ? storage.removeItem(key)
+    : storage.setItem(key, JSON.stringify(val));
+}
+
+/**
+ * Get `key`.
+ *
+ * @param {String} key
+ * @return {Mixed}
+ */
+
+function get(key){
+  return null == key
+    ? storage.clear()
+    : unserialize(storage.getItem(key));
+}
+
+/**
+ * Get all.
+ *
+ * @return {Object}
+ */
+
+function all(){
+  var len = storage.length
+    , ret = {}
+    , key
+    , val;
+
+  for (var i = 0; i < len; ++i) {
+    key = storage.key(i);
+    ret[key] = get(key);
+  }
+
+  return ret;
 }
 
 });
@@ -5411,7 +5411,7 @@ Retsly.prototype.request = function(method, url, query, cb) {
   }
 
   var endpoint = getDomain() + url + '?' + getQuery(options.query);
-  var data = (options.body && options.body.length && typeof options.body !== 'undefined')
+  var data = (method !== 'get' && options.body && typeof options.body !== 'undefined')
     ? JSON.stringify(options.body)
     : '';
 
