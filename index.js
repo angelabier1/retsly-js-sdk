5adc18dcd56b65324377aaf0c9ff20e1108bb0a1
Phil Jepsen <phil@wired8.com>
2015-03-18 16:05:04 -0700

    Revert "Set default root cookie path for setUserToken"
    This reverts commit 360097647a5d20253ecc944ca733aa32c81040f7.

--- index.js
+++ index.js
@@ -286,7 +286,7 @@ Retsly.prototype.getToken = Retsly.prototype.getAppToken = function() {
  */
 Retsly.prototype.setUserToken = function(token) {
   this.token = token;
-  store('retsly.uid', token, { path: '/' });
+  store('retsly.uid', token);
   return this;
 };
 
